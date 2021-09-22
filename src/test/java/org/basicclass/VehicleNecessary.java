f8ee213a714067dba126c273de7ad3db2533d3ad
prakashjothi <prakashjothi22@gmail.com>
2021-09-22 06:25:33 +0530

    Vehicles

---
+++ src/test/java/org/basicclass/VehicleNecessary.java
@@ -0,0 +1,22 @@
+package org.basicclass;
+
+public class VehicleNecessary {
+
+	public void allVehicles() {
+		TwoWheeler tw = new TwoWheeler();
+		ThreeWheeler th = new ThreeWheeler();
+		
+		tw.bike();
+		tw.cycle();
+		
+		th.car();
+		th.bus();
+		th.lorry();
+
+	}
+	
+	
+	
+	
+
+}
