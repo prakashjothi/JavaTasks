45dead5548de05f82896b8f15306f2e02faa93c1
prakashjothi <prakashjothi22@gmail.com>
2021-09-22 05:52:55 +0530

    JavaTasks to Git

---
+++ src/test/java/org/basicclass/BasicClass.java
@@ -0,0 +1,30 @@
+package org.basicclass;
+
+import org.testng.annotations.Test;
+
+public class BasicClass {
+	
+	@Test
+	private void employee() {
+		
+		EmployeeDetails ed = new EmployeeDetails();
+		ed.setEmpId(123);
+		ed.setEmpName("Prakash");
+		ed.setEmpAddress("Chromepet");
+		ed.setEmpDob("01.04.1987");
+		ed.setEmpPhone(9884545801l);
+		ed.setEmpEmail("prakashjothi22@gmail.com");
+		
+		System.out.println(ed.getEmpId());
+		System.out.println(ed.getEmpName());
+		System.out.println(ed.getEmpDob());
+		System.out.println(ed.getEmpPhone());
+		System.out.println(ed.getEmpAddress());
+		System.out.println(ed.getEmpEmail());
+		
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/org/basicclass/EmployeeDetails.java
@@ -0,0 +1,51 @@
+package org.basicclass;
+
+public class EmployeeDetails {
+	
+	private int empId; 
+	private String empName;
+	private String empDob;
+	private long empPhone;
+	private String empEmail;
+	private String empAddress;
+	
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public void setEmpDob(String empDob) {
+		this.empDob = empDob;
+	}
+	public void setEmpPhone(long empPhone) {
+		this.empPhone = empPhone;
+	}
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public String getEmpDob() {
+		return empDob;
+	}
+	public long getEmpPhone() {
+		return empPhone;
+	}
+	public String getEmpEmail() {
+		return empEmail;
+	}
+	public String getEmpAddress() {
+		return empAddress;
+	}
+	
+		
+
+}
